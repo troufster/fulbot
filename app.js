1dfeba515e0974938552ad27c8691d205222e73a
Stefan Pataky <stefan.pataky@gmail.com>
2012-10-11 21:17:10 +0200

    Yeah

--- app.js
+++ app.js
@@ -1,34 +1,19 @@
-#!/usr/bin/env node
-
 var irc = require("irc");
-var Listener = require('./listeners').Listener;
-var chanlisteners = new Listener();
+var Listener = require("./listeners").Listener;
 
-
-
+//Todo: Move to conf.json
 var conf = {
   server : "irc.isolated.se",
-  channels : ["#martin"],
-  nick : "Martin"
+  channels : ["#botdev", "#martin"],
+  nick : "Ulla2"
 };
+
 
 var bot = new irc.Client(conf.server, conf.nick, {
   debug: true,
   channels : conf.channels
 });
 
-chanlisteners.loadPlugins(null,null, false);
-chanlisteners.init(bot);
-
-bot.addListener("message", function(from, to, message) {
-  console.log(to);
-  if( to.match(/^[#&]/)) {
-    //Chan msg
-    chanlisteners.checkListeners(from, to, message);
-  } else {
-    //Priv msg
-  }
-});
-
+var listeners = new Listener(bot);
 
 
