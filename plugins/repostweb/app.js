cf2c947adfddae17359863235f1297a6ac19ca17
troufster <stefan.pataky@gmail.com>
2015-02-20 14:07:02 +0100

    info page

--- plugins/repostweb/app.js
+++ plugins/repostweb/app.js
@@ -13,20 +13,56 @@ configMixin(RepostStore);
 
 var r = new RepostStore();
 var data;
+var urls;
 
-r.load('reposts', resfile, function(e, d) {
-  if (e) {
-    throw e;
-  }
+function load() {
+  r.load('reposts', resfile, function(e, d) {
+    if (e) {
+      throw e;
+    }
 
-  data = d;
-});
+    data = d;
+    urls = Object.keys(data._urls);
+  });
+}
+
+load();
+
+setInterval(function () {
+  load();
+}, 100000);
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
 app.get('/', function (req, res) {
-  res.render('index', { title: 'Reposts', urls: Object.keys(data._urls).reverse(), data: data});
+
+  var urlz = Array.prototype.slice.call(urls);
+
+  res.render('index', { title: 'Reposts', urls: urlz.reverse(), data: data});
+});
+
+app.get('/repost/:id', function(req,res) {
+
+  var id = req.params.id;
+
+  if(!id) {
+    return;
+  }
+
+  var key = urls[id];
+
+  if(!key) {
+    return;
+  }
+
+  var repost = data._urls[key];
+
+  if(!repost) {
+    return;
+  }
+
+  res.render('repost', { repost: repost, url : key, id : id});
 });
 
 var server = app.listen(14140, function(){
